44f650b4042255f2e5569b37c7be71d65097d087
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-07-28 18:18:58 -0700

    Update AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -63,7 +63,7 @@ describe("About Applying What We Have Learnt", function() {
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
+    
     expect(233168).toBe(sum);
   });
 
